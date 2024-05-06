0572898819151d749508b34a97cfec1a3482b328
Leonel Chauca <leonelchauca0@gmail.com>
2024-05-05 20:19:33 -0400

    Agregar archivos de proyecto.

---
+++ Ejer9/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ejer9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient();
+            ws.AgregarPersona(textBox1.Text,textBox2.Text,textBox3.Text,textBox4.Text,textBox5.Text,textBox6.Text);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient();
+            ws.EliminarPersona(Convert.ToInt16(textBox7.Text));
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox14_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient();
+            ws.ModificarPersona(Convert.ToInt16(textBox14.Text),textBox13.Text,textBox12.Text,textBox11.Text,textBox10.Text,textBox9.Text,textBox8.Text);
+        }
+
+    }
+}
